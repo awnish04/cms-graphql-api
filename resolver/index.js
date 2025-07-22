ff81cb7917fadf8c9113f735212e85b32956df55
AWNISH MEHTA <awnishmehta@AWNISHs-MacBook-Air.local>
2025-07-22 22:14:42 +0545

    Updated resolver and typeDefs for project

--- resolver/index.js
+++ resolver/index.js
@@ -1,27 +1,3 @@
-// import About from "../models/About.js";
-
-// export const resolvers = {
-//   Query: {
-//     getAbout: async () => await About.find(),
-//   },
-//   Mutation: {
-//     createAbout: async (_, { heading, paragraph, imageUrl }) => {
-//       return await About.create({ heading, paragraph, imageUrl });
-//     },
-//     deleteAbout: async (_, { id }) => {
-//       return await About.findByIdAndDelete(id);
-//     },
-//     //New Update
-//     updateAbout: async (_, { id, heading, paragraph, imageUrl }) => {
-//       return await About.findByIdAndUpdate(
-//         id,
-//         { heading, paragraph, imageUrl },
-//         { new: true }
-//       );
-//     },
-//   },
-// };
-
 import About from "../models/About.js";
 import Project from "../models/Project.js"; // 🆕
 
