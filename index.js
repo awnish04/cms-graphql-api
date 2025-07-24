592c620df2b01fa5258cbe1a6f862303d3e536ff
AWNISH MEHTA <awnishmehta@AWNISHs-MacBook-Air.local>
2025-07-24 18:11:39 +0545

    chore: remove deprecated mongoose options (useNewUrlParser, useUnifiedTopology)

--- index.js
+++ index.js
@@ -110,10 +110,7 @@ const startServer = async () => {
 
   // ✅ MongoDB connection
   mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => {
       app.listen({ port: PORT }, () => {
         console.log(
